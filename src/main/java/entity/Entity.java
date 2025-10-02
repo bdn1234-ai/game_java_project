cc03d98b4adced2d70be88be87b3bbbdcf522f8a
huyicetea <huy7trung@gmail.com>
2025-10-02 22:04:41 +0700

    player using Huster skin

--- src/main/java/entity/Entity.java
+++ src/main/java/entity/Entity.java
@@ -8,9 +8,9 @@ public class Entity {
     public int speed;
     public String direction;
 
-    public BufferedImage up1, up2, down1, down2, left1, left2, right1, right2;
+    public BufferedImage up1, up2, up3, down1, down2, down3, left1, left2, right1, right2;
     public int spriteCounter = 0;
-    public int spriteNum = 1;
+    public int spriteNum = 3;
 
     public Rectangle solidArea;
     public boolean collisionOn = false;
